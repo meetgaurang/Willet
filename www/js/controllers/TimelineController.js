d03176caadbfba9f6c78bfa00ec202cc375e8c25
Gaurang Patel <meetgaurang@gmail.com>
2016-06-26 01:05:43 +0530

    Working on a timeline page

--- www/js/controllers/TimelineController.js
+++ www/js/controllers/TimelineController.js
@@ -1,5 +1,6 @@
 angular.module('appwillet.controllers')
-.controller('TimelineController', ['$scope', 'getTimelineDataService', function($scope, getTimelineDataService) {
+.controller('TimelineController', ['$scope', 'getTimelineDataService', '$ionicPopup', 
+	function($scope, getTimelineDataService, $ionicPopup) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -7,12 +8,31 @@ angular.module('appwillet.controllers')
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+	$scope.filterSelection = {
+		"education": false,
+		"work-experience": true,
+		"bon-voyage": false,
+		"certifications": false
+	};
 	getTimelineDataService.getTimelineData().then(function successCallback(response){
   		$scope.timelineData = response.data.timelineData;
   	},
   	function errorCallback(error){
   		alert("Error..");
   	});
+
+  	$scope.showAlert = function() {
+	   var alertPopup = $ionicPopup.alert({
+	     //title: 'Don\'t eat that!',
+	     cssClass: 'timeline-filter-modal',
+	     templateUrl: 'partials/timelinefiltermodal.html'
+	   });
+
+	   alertPopup.then(function(res) {
+	     console.log('Thank you for not eating my delicious ice cream cone');
+	   });
+	 };
 
 
 }]);
