1d8084e1254998400f9a80d66967876277f8e496
Gaurang Patel <meetgaurang@gmail.com>
2016-08-08 01:53:55 +0530

    Now, running 'ionicBuildAndroid.bat prod' directly prepares release APK - eady to be moved to Google Play Store

--- hooks/after_build/01_rename_apk.js
+++ hooks/after_build/01_rename_apk.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-
-var fs = require('fs');
-fs.rename("platforms/android/build/outputs/apk/android-debug.apk", "platforms/android/build/outputs/apk/Willet.apk", function(error) {
-	if(error) {
-		console.log("Error: " + error);
-	}
-});
+if (process.env.TARGET!='"prod"') {
+	var fs = require('fs');
+	fs.rename("platforms/android/build/outputs/apk/android-debug.apk", "platforms/android/build/outputs/apk/willet-debug.apk", function(error) {
+		if(error) {
+			console.log("Error: " + error);
+		}
+	});
+}
--- ionicBuildAndroid.bat
+++ ionicBuildAndroid.bat
@@ -1,8 +1,11 @@
 IF "%1"=="prod" (
 	SET TARGET="prod"
 	echo "######### Production Build ############"
+	ionic build android --release
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+	%ANDROID_HOME%/build-tools/24.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/willet-release.apk
 ) ELSE (
 	SET TARGET="dev"
 	echo "######### Development Build ############"
+	ionic build android
 )
-ionic build android
